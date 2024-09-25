9da97071d8d1db008402f774ebe75a6374c23f95
Arkaprava Jana <arkaprava.jana@icloud.com>
2024-09-25 11:07:25 +0200

    Adding parameters and narrowing to active deployments

--- get_k8s_resources.sh
+++ get_k8s_resources.sh
@@ -1,30 +1,57 @@
 #!/bin/bash
 
+# Function to display usage
+usage() {
+    echo "Usage: $0 --namespaces <namespace1,namespace2,...> --resource_types <resource1,resource2,...>"
+    exit 1
+}
+
+# Parse named parameters
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+        --namespaces) namespaces="$2"; shift ;;
+        --resource_types) resource_types="$2"; shift ;;
+        *) usage ;;
+    esac
+    shift
+done
+
+# Check if both parameters are provided
+if [ -z "$namespaces" ] || [ -z "$resource_types" ]; then
+    usage
+fi
+
 # Directory to save the YAML files
 OUTPUT_DIR="k8s_resources"
 mkdir -p $OUTPUT_DIR
 
-# Get all namespaces
-namespaces=$(kubectl get namespaces -o jsonpath='{.items[*].metadata.name}')
+# Convert comma-separated strings to arrays
+IFS=',' read -r -a namespace_array <<< "$namespaces"
+IFS=',' read -r -a resource_type_array <<< "$resource_types"
 
 # Loop through each namespace
-for ns in $namespaces; do
+for ns in "${namespace_array[@]}"; do
     # Create a directory for the namespace
     ns_dir="$OUTPUT_DIR/$ns"
     mkdir -p $ns_dir
 
-    # Get all resource types in the namespace
-    resource_types=$(kubectl api-resources --verbs=list --namespaced -o name)
-
     # Loop through each resource type
-    for resource in $resource_types; do
+    for resource in "${resource_type_array[@]}"; do
         # Skip secrets resource type
         if [ "$resource" == "secrets" ]; then
             continue
         fi
 
         # Get all resources of this type in the namespace
-        resources=$(kubectl get $resource --namespace $ns -o jsonpath='{range .items[*]}{.metadata.name}{"\n"}{end}')
+        if [ "$resource" == "replicaset" ]; then
+            # Exclude inactive ReplicaSets
+            resources=$(kubectl get $resource --namespace $ns -o jsonpath='{range .items[?(@.status.replicas==@.status.readyReplicas)]}{.metadata.name}{"\n"}{end}')
+        elif [ "$resource" == "deployment" ]; then
+            # Exclude inactive Deployments
+            resources=$(kubectl get $resource --namespace $ns -o jsonpath='{range .items[?(@.status.replicas>0)]}{.metadata.name}{"\n"}{end}')
+        else
+            resources=$(kubectl get $resource --namespace $ns -o jsonpath='{range .items[*]}{.metadata.name}{"\n"}{end}')
+        fi
 
         # Loop through each resource and save to a separate YAML file
         for res in $resources; do
